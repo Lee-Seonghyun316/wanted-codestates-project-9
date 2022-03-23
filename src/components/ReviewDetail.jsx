e1a3793c306ecc5129dedf3c48d3bcf166ac2eea
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-03-23 12:43:34 +0900

    Design: reviewDetail header ui 추가

--- src/components/ReviewDetail.jsx
+++ src/components/ReviewDetail.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const ReviewDetail = () => {
-  return <div>reviewDetail</div>;
+  return (
+    <div>
+      <Head>
+        <button>
+          <ButtonImg src="http://djp5oonlusoz4.cloudfront.net/contents/event/20190924/ic_left_btn.png" alt="left" />
+        </button>
+        리뷰 상세보기
+        <button>
+          <ButtonImg src="https://djp5oonlusoz4.cloudfront.net/contents/event/20190924/ic_can_btn.png" alt="left" />
+        </button>
+      </Head>
+    </div>
+  );
 };
 
 export default ReviewDetail;
+
+const Head = styled.header`
+  position: fixed;
+  text-align: center;
+  padding: 1.4rem 0;
+  top: 0;
+  box-shadow: 0 0px 3px 0px #ccc;
+  width: 100%;
+  background: #fff;
+  z-index: 999;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+  font-family: Lato;
+  color: #020202;
+  letter-spacing: 0.031rem;
+  font-size: 1.4rem;
+  font-weight: 700;
+  white-space: nowrap;
+`;
+
+const ButtonImg = styled.img`
+  width: 1.5rem;
+  margin: 0.3rem 1.6rem 0;
+`;
